7736a7b25325356640c9477021d7aeeafdac6f73
kirstiecc2025-alt <kirsti.ecc2025@gmail.com>
2026-07-23 21:41:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lantern-academy-v8-3.2-clearer-path";
+const CACHE = "lantern-academy-v9-3.3-guided-lantern";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
